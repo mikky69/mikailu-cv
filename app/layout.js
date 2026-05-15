1c846a39c9c867905722a640cd30dfbf25cd838b
Mikailu Nadro <74379292+mikky69@users.noreply.github.com>
2026-05-15 19:22:48 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Engr. Mikailu Samuel Nadro - CV',
-  description: 'Professional CV of COREN Registered Civil/Structural Engineer',
+  title: "Engr. Mikailu Nadro | COREN Registered Structural Engineer",
+  description: "Professional CV of Engr. Mikailu Samuel Nadro, COREN-registered Civil/Structural Engineer specializing in estate developments & structural design in Nigeria.",
 }
 
 export default function RootLayout({ children }) {
